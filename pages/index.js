5cc19e5c8c1d1466cf2778102ea2a0068ceb7b7d
mmaksi <markmaksi@outlook.com>
2022-09-08 09:42:10 +0300

    Adding featured posts cards on top of the home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
+import FeaturedPosts from "../sections/FeaturedPosts"
 
 export default function Home ({ posts }) {
   return (
@@ -9,7 +10,7 @@ export default function Home ({ posts }) {
         <title>mBlog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <FeaturedPosts />
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="lg:col-span-8 col-span-1">
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -8,17 +8,17 @@ import {
   Author,
   Comments,
   CommentsForm,
-  // Loader,
+  Loader,
 } from "../../components";
 import { getPosts, getPostDetails } from "../../services";
-// import { AdjacentPosts } from "../../sections";
+import { AdjacentPosts } from "../../sections";
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
 
-  // if (router.isFallback) {
-  //   return <Loader />;
-  // }
+  if (router.isFallback) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -27,7 +27,7 @@ const PostDetails = ({ post }) => {
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
-            {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
